c1f450127564a66d9f35b1ec776a5b9283816e05
Milena <105459743+carvmi@users.noreply.github.com>
2023-10-28 18:11:12 -0300

    Add files via upload

---
+++ spring-boot-example/src/main/java/com/carvmidev/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.carvmidev;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface CustomerRepository
+        extends JpaRepository<Customer, Integer>
+{
+}
